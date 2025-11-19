8a4fe5c06ef7d303e5461a598a1f9bd041717075
gambinoxxx <ibehchristopher11@gmail.com>
2025-11-18 20:20:14 -0800

    added proxyUrl to the layout .jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <body className={`${outfit.className} antialiased`}>
                 <ClerkProvider
+                    proxyUrl="https://accounts.bopstore.com.ng"
                     appearance={{
                         elements: {
                             modalBackdrop: "bg-black/50",
