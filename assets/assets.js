981f0039dd0b678d1cf9f1f3eddb3e94eaf3d3b8
ibeh christopher <109024548+gambinoxxx@users.noreply.github.com>
2026-03-05 11:16:44 +0100

    Add service image to assets

--- assets/assets.js
+++ assets/assets.js
@@ -30,9 +30,11 @@ import { ClockFadingIcon, HeadsetIcon, SendIcon } from "lucide-react"
 import profile_pic1 from "./profile_pic1.jpg"
 import profile_pic2 from "./profile_pic2.jpg"
 import profile_pic3 from "./profile_pic3.jpg"
+import service from "./service-pro.png"
+
 
 export const assets = {
-    upload_area, hero_model_img, hero_product_img1, hero_product_img2, gs_logo, product_img1, product_img2, product_img3, product_img4, product_img5, product_img6, product_img7, product_img8, product_img9, product_img10, product_img11, product_img12, img1, img2, img3, img4, img5, img6,img7, img8, img9, img10
+    upload_area, hero_model_img, service, hero_product_img1, hero_product_img2, gs_logo, product_img1, product_img2, product_img3, product_img4, product_img5, product_img6, product_img7, product_img8, product_img9, product_img10, product_img11, product_img12, img1, img2, img3, img4, img5, img6,img7, img8, img9, img10
 }
 
 export const categories = ["Headphones", "Speakers", "Watch", "Earbuds", "Mouse", "Decoration"];
