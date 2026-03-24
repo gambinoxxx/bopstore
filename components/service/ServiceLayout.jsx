c2b82e497010b181765809fa6b60f292614d390b
ibeh christopher <109024548+gambinoxxx@users.noreply.github.com>
2026-03-24 15:34:17 +0100

    Add ServiceLayout component with DashboardLayout

--- components/service/ServiceLayout.jsx
+++ components/service/ServiceLayout.jsx
@@ -1,1 +1,20 @@
+'use client'
+import DashboardLayout from '@/components/DashboardLayout'
+import ServiceNavbar from './ServiceNavbar'
+import ServiceSidebar from './ServiceSidebar'
 
+const ServiceLayout = ({ children }) => {
+    return (
+        <DashboardLayout
+            apiEndpoint="/api/service/is-provider"
+            roleKey="isProvider"
+            infoKey="serviceInfo"
+            Navbar={ServiceNavbar}
+            Sidebar={ServiceSidebar}
+        >
+            {children}
+        </DashboardLayout>
+    )
+}
+
+export default ServiceLayout
